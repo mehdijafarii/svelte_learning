fcb5a2fcd3e2a0d0cda74d2b29ac43f608755078
mahdi <mj.se@outlook.com>
2023-01-08 11:44:07 +0000

    conditional css

--- 6-10/src/Modal.svelte
+++ 6-10/src/Modal.svelte
@@ -1,9 +1,10 @@
 <script>
-  let showModel = false;
+  let showModel = true;
+  let isPromo = false;
 </script>
 
 {#if showModel}
-  <div class="backdrop">
+  <div class="backdrop" class:promo={isPromo}>
     <div class="modal">
       <p>Sign up for offers!!</p>
     </div>
@@ -11,4 +12,24 @@
 {/if}
 
 <style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgb(0, 0, 0, 0.8);
+  }
+
+  .modal {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10px auto;
+    text-align: center;
+    background: white;
+  }
+
+  .promo .modal {
+    background: crimson;
+    color: white;
+  }
 </style>
